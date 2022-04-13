397dfd8f6295711a87f2af3a8a140bd8bffd9ff0
Consuelo Garrido Abedrapo <consuelogarrido.ab@gmail.com>
2022-04-13 19:10:49 -0400

    add data to cards

--- script.js
+++ script.js
@@ -1,43 +1,20 @@
 $(document).ready(function () {
+  
 
     const urlPokemons = 'https://pokeapi.co/api/v2/pokemon/';
 
+    
     getPokemons(urlPokemons);
-
+    
     $('#getMorePokemons').click(function () {
         const urlNext = $(this).attr('nextPageUrl');
         getPokemons(urlNext);
     });
-    console.log('hola')
     
-    
+   
 });
 
-const getPokemons = (url) => {
-    
-    fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        const pokemons = data.results;
-        const urlMorePokemons = data.next;
-
-        $('#getMorePokemons').attr('nextPageUrl', urlMorePokemons);
-        pokemons.forEach(function (pokemon) {
-            showPokemon(pokemon);
-        });
-
-        $('.btnGetDataPokemon').click(function () {
-            const urlPokemon = $(this).attr('data-url-pokemon');
-            getPokemonData(urlPokemon);
-        });
-        
-      
-    });
-    
-};
-
 const getPhoto = (url, name) => {
-    console.log('obteniendo fotos ' + url)
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
@@ -45,69 +22,81 @@ const getPhoto = (url, name) => {
         });
 }
 
-const getPokemonData = (url) => {
-    console.log('obteniendo datos desde' + url);
+const getPokemons = (url) => {
+
 
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-            $('#modalPokemonLabel').text(data.name);
+            const pokemons = data.results;
+            const urlMorePokemons = data.next;
 
-            console.log("PokemonData", data);
+            $('#getMorePokemons').attr('nextPageUrl', urlMorePokemons);
 
-            $('#pokemonAbilities').text('');
-            $('#pokemonMoves').text('');
+            pokemons.forEach(function (pokemon) {
+                showPokemon(pokemon, pokemon.url);
+                console.log(pokemon.url)
+            });            
+
+            $('.btnGetDataPokemon').click(function () {
+                const urlPokemon = $(this).attr('data-url-pokemon');
+                getPokemonData(urlPokemon);
+                
+            });
+        });
+};
+
+const getPokemonData = (url) => {
 
     
-            data.abilities.forEach(function (ability) {
-                $('#pokemonAbilities').append(`<li>${ability.ability.name}</li>`);
-            });
-        
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            $('#modalPokemonLabel').text(data.name);
+            $('#pokemonAbilities').text('');
+            $('#pokemonMoves').text('');
+            $('#pokemonWeight').text('');
+            $('#pokemonBaseExperience').text('');
+               
+            $('#pokemonAbilities').append(`<li>${data.abilities[0].ability.name}</li>`);
+       
             $('#pokemonMoves').append(`<li>${data.moves[0].move.name}</li>`);
-         
 
+            $('#pokemonWeight').append(`<li>${data.weight}</li>`);
+
+            $('#pokemonBaseExperience').append(`<li>${data.base_experience}</li>`);
+         
             $("#modalPokemon").modal("show");
+         
         });
 
 }
 
 
-const showPokemon = (pokemon) => {
 
-    $('#pokedex').append(`
-        
+const showPokemon = (pokemon, url) => {
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            $('#pokedex').append(`
             <div class='card col-6'>
                 <div class='card-body'>
-                    <h4 class='card-title'>${pokemon.name}</h4>
+                    <h5 class='card-title'>${pokemon.name}</h5>
                     <div class = "d-inline-block">
                         <img src="" id="img_${pokemon.name}" class = "img-card">
                     </div>
-                    <div class = "d-inline-block">
-                        <ul>
-                            <li> "Weight : ${pokemon.name.height}" </li>
-                            <li> "Ability : ${pokemon.name.ability}" </li>
-                        </ul>   
-                        <a href = "pokemon.html" class = "btnGetDataPokemon" >
-                            <button class ="btn btn-primary " >¡mas info!</button> 
-                        </a>
+                    <div id="datosCard" class="d-inline-block">
+                        <p>Peso: <ul><li>${data.weight}</li></ul> </p>
+                        <p>Habilidad: <ul><li>${data.abilities[0].ability.name}</li></ul> </p>
                     </div>
+                    <button class ="btn btn-primary btnGetDataPokemon" data-url-pokemon = "${pokemon.url}">¡Quiero saber más de este pokémon!</button>
                 </div>
             </div>
-           
-    `);
-    getPhoto(pokemon.url, pokemon.name);
-}
+            `);
+});
 
-const showPokemonData = (pokemon) => {
-
-    $('#pokemonData').append(`
-        
-    <div>
-        <img id = "img_${pokemon.name}" class = "w-100">
-    </div>
     
-           
-    `);
-    getPhoto(pokemon.url, pokemon.name);
+getPhoto(pokemon.url, pokemon.name)
     
 }
